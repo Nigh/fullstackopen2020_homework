bed9f556e0067ea65ae023139ef2c3443d66be67
nigh <jiyucheng007@gmail.com>
2020-07-02 16:55:02 +0800

    exercises 1.5 commit
    uses array to handle the content
    
    Signed-off-by: nigh <jiyucheng007@gmail.com>

--- part1/Ep.a/src/index.js
+++ part1/Ep.a/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = (p) => (
-	<h1>{p.course}</h1>
+	<h1>{p.course.name}</h1>
 )
 
 const Part = (p) => (
@@ -31,27 +31,29 @@ const Total = (p) => {
 }
 
 const App = () => {
-	const course = 'Half Stack application development'
-	const parts = [
-		{
-			name: 'Fundamentals of React',
-			exercises: 10
-		},
-		{
-			name: 'Using props to pass data',
-			exercises: 7
-		},
-		{
-			name: 'State of a component',
-			exercises: 14
-		}
-	]
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				exercises: 10
+			},
+			{
+				name: 'Using props to pass data',
+				exercises: 7
+			},
+			{
+				name: 'State of a component',
+				exercises: 14
+			}
+		]
+	}
 
 	return (
 		<>
 			<Header course={course} />
-			<Content parts={parts} />
-			<Total parts={parts} />
+			<Content parts={course.parts} />
+			<Total parts={course.parts} />
 		</>
 	)
 }
