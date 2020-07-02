a08115ee6904c18785ed6f52f0c7a8541018e56a
nigh <jiyucheng007@gmail.com>
2020-07-02 15:52:47 +0800

    exercises 1.2 commit
    Signed-off-by: nigh <jiyucheng007@gmail.com>

--- part1/Ep.a/src/index.js
+++ part1/Ep.a/src/index.js
@@ -5,10 +5,24 @@ const Header = (p) => (
 	<h1>{p.course}</h1>
 )
 
+const Part = (p) => (
+	<p>{p.name}:{p.num}</p>
+)
+
+const course = 'Half Stack application development'
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
+
 const Content = (p) => (
-	<p>
-		{p.name}:{p.num}
-	</p>
+	<div>
+		<Part name={part1} num={exercises1} />
+		<Part name={part2} num={exercises2} />
+		<Part name={part3} num={exercises3} />
+	</div>
 )
 
 const Total = (p) => (
@@ -16,20 +30,10 @@ const Total = (p) => (
 )
 
 const App = () => {
-	const course = 'Half Stack application development'
-	const part1 = 'Fundamentals of React'
-	const exercises1 = 10
-	const part2 = 'Using props to pass data'
-	const exercises2 = 7
-	const part3 = 'State of a component'
-	const exercises3 = 14
-
 	return (
 		<>
 			<Header course={course} />
-			<Content name={part1} num={exercises1} />
-			<Content name={part2} num={exercises2} />
-			<Content name={part3} num={exercises3} />
+			<Content/>
 			<Total total={exercises1 + exercises2 + exercises3} />
 		</>
 	)
